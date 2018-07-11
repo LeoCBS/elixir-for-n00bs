accc9f9da4793d9a87cec48b33a9aacfac939a29
Leonardo Cesar Borges <leonardo.borges@neoway.com.br>
2018-07-11 09:34:27 -0300

    test func div

---
+++ hello_world/test/hello_world_test.exs
@@ -0,0 +1,17 @@
+defmodule HelloWorldTest do
+  use ExUnit.Case
+  doctest HelloWorld
+
+  test "greets the world" do
+    assert HelloWorld.hello() == :world
+  end
+
+  test "division" do
+    {:ok, result} = HelloWorld.div(2,1)
+    assert result == 2.0
+  end
+  test "division by zero" do
+    {:error, err} = HelloWorld.div(1,0)
+    assert err == "attempt at division by zero"
+  end
+end
