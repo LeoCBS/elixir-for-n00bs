5fbaa2d188ceea3c379fa95a9d22a6b5e6140b7f
Leonardo Cesar Borges <leonardo.borges@neoway.com.br>
2018-07-11 08:14:46 -0300

    finish types examples

--- types.exs
+++ types.exs
@@ -26,14 +26,29 @@ new_person = put_elem(person, 2 ,31)
 age = elem(new_person, 2)
 IO.puts "Age #{age}" 
 
-
 #Collections
-#List
 
 #append
-l= [1 | [2,3]]
+list_example = [1 | [2,3]]
 
-IO.inspect l
-IO.inspect hd(l)
-IO.inspect tl(l)
+IO.inspect list_example
+IO.inspect hd(list_example)
+IO.inspect tl(list_example)
+
+keyword_list = [{:leo,"elixir"}]
+
+IO.inspect keyword_list
+
+map_example = %{foo: "bar"}
+
+IO.inspect map_example.foo
+
+#anonymous functions
+
+add_two_func = fn x -> x+2 end
+
+IO.inspect add_two_func.(2)
+
+
+
 
