f160324c4294a71f5a21d632fdf53252130ba429
Leonardo Cesar Borges <leonardo.borges@neoway.com.br>
2018-07-22 18:27:03 -0300

    simple macro example

---
+++ macros.exs
@@ -0,0 +1,21 @@
+defmodule MyLib do
+  defmacro test(arg) do
+    arg |> IO.inspect
+  end
+  defmacro testunquoet(arg) do
+  quote do
+    unquote(arg) + 1
+  end
+end
+end
+
+defmodule Main do
+  require MyLib
+ 
+  def start! do
+    MyLib.test({1,2,3})
+  end
+end
+ 
+
+Main.start!
