37467b54c5d3a2a9b9a2411d2ae4e4a093afe99c
Leonardo Cesar Borges <leonardo.borges@neoway.com.br>
2018-07-22 19:39:04 -0300

    add more macro example

--- macros.exs
+++ macros.exs
@@ -2,9 +2,14 @@ defmodule MyLib do
   defmacro test(arg) do
     arg |> IO.inspect
   end
+  
   defmacro testunquoet(arg) do
   quote do
     unquote(arg) + 1
+  end
+
+  def if_palindrome_f?(str, expr) do
+      if str == String.reverse(str), do: expr
   end
 end
 end
@@ -15,6 +20,7 @@ defmodule Main do
   def start! do
     MyLib.test({1,2,3})
     MyLib.testunquoet(1 + 2) |> IO.inspect
+    #MyLib.if_palindrome_f?("745", IO.puts("yes"))
   end
 end
  
